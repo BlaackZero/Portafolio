3edb0566b3f0715583b9b724339c26a82e3fee04
unknown <blaackzeero@gmail.com>
2022-10-18 13:38:07 -0300

    GOD

--- Portafolio/src/app/app-routing.module.ts
+++ Portafolio/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [
-  { path: 'menu', component: MenuComponent }
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Portafolio/src/app/app.module.ts
+++ Portafolio/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { ContenidoComponent } from './contenido/contenido.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    ContenidoComponent
+    ContenidoComponent,
   ],
   imports: [
     BrowserModule,
