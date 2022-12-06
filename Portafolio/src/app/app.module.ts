857379eb03f9344f0a0dd20bdc79988bb1d72de7
BlaackZero <blaackzeero@gmail.com>
2022-12-06 14:13:13 -0300

    Commit

--- Portafolio/src/app/app.module.ts
+++ Portafolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContenidoComponent } from './contenido/contenido.component';
+import { TrabajosComponent } from './trabajos/trabajos/trabajos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContenidoComponent,
+    TrabajosComponent,
   ],
   imports: [
     BrowserModule,
